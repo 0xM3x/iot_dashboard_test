1e9ba18403b8ae7822dcca67e7335b280ac34b8a
0xM3x <m3x.m3x.proton.me>
2025-03-21 02:44:54 +0300

    first working v1.0 (server side)

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,18 +1,33 @@
-const client = mqtt.connect('ws://185.122.200.9:9001');
+const client = mqtt.connect('ws://185.122.200.9:8083/mqtt');
+
 
 client.on('connect', function () {
     console.log('connected to MQTT');
-    client.subscribe("esp32/led/status");
+    client.subscribe("esp32/led/status", function (err) {
+        if (err) {
+                console.log('Subscription error: ', err);
+        } else {
+                console.log('sucess ...');
+        }
+    });
 });
 
 client.on('message', function (topic, message) {
-    document.getElementById('status').innerText = message.toString();
+    if (topic === 'esp32/led/status') {
+        const ledStatus = message.toString();
+        console.log('LED status: ', ledStatus);
+        document.getElementById('ledStatus').innerText = ledStatus;
+    }
+
 });
 
-document.getElementById('onButton').addEventListener("click", () => {
+
+document.getElementById('turnOn').addEventListener("click", () => {
+    console.log('Turning LED ON');
     client.publish("esp32/led", "ON");
 });
 
-document.getElementById('offButton').addEventListener("click", () => {
+document.getElementById('turnOff').addEventListener("click", () => {
+    console.log('Turning LED OFF');
     client.publish("esp32/led", "OFF");
 });
--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const mqtt = require("mqtt");
 const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+const mqttClient = mqtt.connect('mqtt://185.122.200.9:1883');
+
+mqttClient.on('connect', () => {
+        console.log("connected tp MQTT broker");
+        mqttClient.subscribe('esp32/led/status', (err) => {
+                if (err) {
+                        console.log('subscription error: ', err);
+                }
+        });
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
